ce38a803afbe9647735ccd39aba0c33c36d344ce
Onii Chan <46487694+sogood99@users.noreply.github.com>
2023-11-01 12:53:03 -0400

    add idt header file

--- cpu/idt.h
+++ cpu/idt.h
@@ -19,6 +19,9 @@ struct idt_ptr_struct {
 } __attribute__((packed));
 typedef struct idt_ptr_struct idt_ptr_t;
 
-extern void isr0(), isr1();
+extern void isr0(), isr1(), isr2(), isr3(), isr4(), isr5(), isr6(), isr7(),
+    isr8(), isr9(), isr10(), isr11(), isr12(), isr13(), isr14(), isr15(),
+    isr16(), isr17(), isr18(), isr19(), isr20(), isr21(), isr22(), isr23(),
+    isr24(), isr25(), isr26(), isr27(), isr28(), isr29(), isr30(), isr31();
 
 #endif
